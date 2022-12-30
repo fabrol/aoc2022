a668ba3c14865f16aa7f2aef3714d2877c4523e4
Farhan Abrol <fabrol@fb.com>
2022-12-29 23:07:38 -0500

    some simplification

--- day8.py
+++ day8.py
@@ -99,6 +99,12 @@ def run_part_a(filename):
     return len(visible)
 
 
+"""
+Goal: Find the closest height h >= current in the scan direction with only heights <= h between h and current
+forward_pass = starting from index 0 and going to max, false = starting from max index
+"""
+
+
 def find_next_highest(val, next_array, forward_pass, default_val, max_height=10):
     next_so_far = None
     for ht in range(val, max_height):
@@ -113,57 +119,67 @@ def find_next_highest(val, next_array, forward_pass, default_val, max_height=10)
     return next_so_far
 
 
+def init_arrays(rows, cols, num_height, init_val):
+    return [[[init_val]*num_height for _ in range(cols)] for _ in range(rows)]
+
+
 def run_part_b(filename):
     lines = read_input(filename)
     g = create_graph(lines)
     rows = len(g)
     cols = len(g[0])
+    size = cols
 
     # [-1,-1,..] array of size 10 showing the last location in that direction for val = idx
     num_height = 10
-    left = [[[-1]*num_height for _ in range(cols)] for _ in range(rows)]
-    right = [
-        [[cols]*num_height for _ in range(cols)] for _ in range(rows)]
-    top = [[[-1]*num_height for _ in range(cols)] for _ in range(rows)]
-    bottom = [[[rows]*num_height for _ in range(cols)] for _ in range(rows)]
+    left = init_arrays(rows, cols, num_height, -1)
+    right = init_arrays(rows, cols, num_height, cols)
+    top = init_arrays(rows, cols, num_height, -1)
+    bottom = init_arrays(rows, cols, num_height, rows)
 
     # Iterate over the graph 4 times, storing state for each direction each time
-    # Reuse indices because we have a square grid
-    for r in range(rows):
-        for c in range(cols):
+    # This is storing away the next closest location from scan direction of each height
+    for major in range(size):
+        for minor in range(size):
             # left to right
-            val = g[r][c]
-            if c > 0:
-                left[r][c] = (left[r][c-1]).copy()
-            if c > left[r][c][val]:
-                left[r][c][val] = c
+            mr, mi = major, minor  # Major, minor
+            val = g[mr][mi]
+            if mi > 0:
+                left[mr][mi] = (left[mr][mi-1]).copy()
+            if mi > left[mr][mi][val]:
+                left[mr][mi][val] = mi
 
             # right to left
-            val = g[r][cols - c - 1]
-            if cols - c - 1 < cols - 1:
-                right[r][cols - c - 1] = (right[r][cols - c]).copy()
-            if (cols - c - 1) < right[r][cols - c - 1][val]:
-                right[r][cols - c - 1][val] = cols-c-1
+            mr, mi = major, size-1-minor
+            val = g[mr][mi]
+            if mi < size - 1:
+                right[mr][mi] = (right[mr][mi+1]).copy()
+            if mi < right[mr][mi][val]:
+                right[mr][mi][val] = mi
 
             # top to bottom
-            val = g[c][r]
-            if c > 0:
-                top[c][r] = top[c-1][r].copy()
-            if c > top[c][r][val]:
-                top[c][r][val] = c
+            mr, mi = minor, major
+            val = g[mr][mi]
+            if mr > 0:
+                top[mr][mi] = top[mr-1][mi].copy()
+            if mr > top[mr][mi][val]:
+                top[mr][mi][val] = mr
 
             # bottom to top
-            val = g[cols - c - 1][r]
-            if cols - c - 1 < cols - 1:
-                bottom[cols-c-1][r] = bottom[cols-c][r].copy()
-            if cols - c - 1 < bottom[cols-c-1][r][val]:
-                bottom[cols-c-1][r][val] = cols - c - 1
+            mr, mi = size-1-minor, major
+            val = g[mr][mi]
+            if mr < size - 1:
+                bottom[mr][mi] = bottom[mr+1][mi].copy()
+            if mr < bottom[mr][mi][val]:
+                bottom[mr][mi][val] = mr
 
+    '''
     print_graph(g)
     print_graph(left)
     print_graph(right)
     print_graph(top)
     print_graph(bottom)
+    '''
 
     res = [[-1]*cols for _ in range(rows)]
     for r in range(rows):
@@ -175,6 +191,7 @@ def run_part_b(filename):
             else:
                 visible = 1
 
+            # Left to right
             if c > 0:
                 next_element = find_next_highest(val, left[r][c-1], True, -1)
                 if not next_element:
@@ -190,15 +207,17 @@ def run_part_b(filename):
                     next_element = cols - 1
                 visible *= (next_element - (c))
 
-            # top to borrom
+            # top to bottom
             if r > 0:
                 next_element = find_next_highest(val, top[r-1][c], True, -1)
                 if not next_element:
                     next_element = 0
                 visible *= (r - next_element)
 
+            # bottom to top
             if r < rows - 1:
-                next_element = find_next_highest(val, bottom[r+1][c], False, cols)
+                next_element = find_next_highest(
+                    val, bottom[r+1][c], False, rows)
                 if not next_element:
                     next_element = rows - 1
                 visible *= (next_element - r)
